a95de1267dfe53f08fa90733eb6151f1a68bf7e2
HansenChristoffer <chris.hansen.ch@outlook.com>
2020-11-25 15:31:34 +0100

    Removed unused methods and imports

--- src/main/java/se/sogeti/app/App.java
+++ src/main/java/se/sogeti/app/App.java
@@ -1,21 +1,16 @@
 package se.sogeti.app;
 
 import java.lang.invoke.MethodHandles;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import se.sogeti.app.config.Constants;
-import se.sogeti.app.scrapers.BaseScraper;
 import se.sogeti.app.scrapers.CategoryScraper;
 
 public class App {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getName());
-	private static Set<BaseScraper> scrapers = new HashSet<>();
-
 
 	public static void main(String[] args) {
 		Constants.init();
@@ -30,28 +25,4 @@ public class App {
 			LOGGER.error("app.Exception == {}", e.getMessage());
 		}
 	}
-
-
-
-
-	private static void sleep(long seconds) {
-		try {
-			Thread.sleep(seconds * 1000);
-		} catch (InterruptedException e) {
-			LOGGER.error("sleep.InterruptedException == {}", e.getMessage());
-			Thread.currentThread().interrupt();
-		}
-	}
-
-	private static void kill() {
-		try {
-			if (!scrapers.isEmpty()) {
-				scrapers.forEach(s -> s.kill());
-				scrapers.clear();
-			}
-		} catch (Exception e) {
-			LOGGER.info("kill().Exception == {}", e.getMessage());
-		}
-	}
-
 }
